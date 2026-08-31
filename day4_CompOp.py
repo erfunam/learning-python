46ec3b07affc60164d5efbdb2cfa40179d4a0620
Seyed Erfan Mohiyodin Jafari <erfunamo@gmail.com>
2026-08-31 23:09:31 +0330

    Day 4:Comparison operators notebook

---
+++ day4_CompOp.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number 2 : "))
+num3 = int(input("Enter number 3 : "))
+"""if (num1 < num2) and (num2 <= num3) :
+    print(True)
+else :
+    print(False)"""
+"""if (num1 < num2 <= num3) :
+    print(True)
+else :
+    print(False)"""
+print(num1 < num2 <= num3)
